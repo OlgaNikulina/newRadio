a1cf6d89bd36a0648e018f6ba83533ca6411737f
OlgaNikulina <nikulinaov@inbox.ru>
2020-03-20 02:48:26 +0500

    try to add constructor

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,37 +1,134 @@
 package ru.netology;
 
 public class Radio {
-    int currentStation = 1;
+    int currentStation = 5;
     int maxStation = 9;
     int minStation = 0;
-    int currentloudness = 5;
-    int maxLoudness = 10;
-    int minLoudness = 1;
+    int currentVolume = 5;
+    int maxVolume = 10;
+    int minVolume = 1;
     boolean on;
     String next;
     String previous;
 
-    public int setStation() {
+    public Radio(int currentStation) {
+        this.currentStation = currentStation;
+    }
+
+
+
+    public int getCurrentStation() {
         if (currentStation == maxStation) {
-            return 0;
+            return minStation;
         }
         if (currentStation == minStation) {
-            return 9;
+            return maxStation;
         }
-
-        currentStation++;
         return currentStation;
     }
 
-    public int setLoudness() {
-        if (currentloudness == maxLoudness) {
-            return 0;
+    public void setCurrentStation(int currentStation) {
+        this.currentStation = currentStation;
+    }
+
+    public int getMaxStation() {
+        return maxStation;
+    }
+
+    public void setMaxStation(int maxStation) {
+        this.maxStation = maxStation;
+    }
+
+    public int getMinStation() {
+        return minStation;
+    }
+
+    public void setMinStation(int minStation) {
+        this.minStation = minStation;
+    }
+
+    public int getCurrentVolume() {
+        if (currentVolume == maxVolume) {
+            return minVolume;
         }
-        if (currentloudness == minLoudness) {
-            return 0;
+        if (currentVolume == minVolume) {
+            return maxVolume;
         }
-        currentloudness++;
-        return currentloudness;
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        this.currentVolume = currentVolume;
+    }
+
+    public int getMaxVolume() {
+        return maxVolume;
+    }
+
+    public void setMaxVolume(int maxVolume) {
+        this.maxVolume = maxVolume;
+    }
+
+    public int getMinVolume() {
+        return minVolume;
+    }
+
+    public void setMinVolume(int minVolume) {
+        this.minVolume = minVolume;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public void setStation() {
+        if (currentStation == maxStation) {
+        }
+
+        if (currentStation == minStation) {
+        }
+
+        currentStation++;
+    }
+
+    public void setVolume() {
+        if (currentVolume == maxVolume) {
+        }
+        if (currentVolume == minVolume) {
+        }
+        currentVolume++;
+    }
+
+    public void nextStation () {
+        if (currentStation == maxStation) {
+        }
+        currentStation += 1;
+            }
+
+    public void prevStation () {
+        if (currentStation == minStation) {
+        }
+        currentStation -= 1;
     }
 }
 
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -8,17 +8,49 @@ class RadioTest {
 
     @Test
     void setStation() {
-        Radio radio = new Radio();
-        int expected = 2;
-        int actual = radio.setStation();
+        Radio radio = new Radio(1);
+        int expected = 1;
+        int actual = radio.getCurrentStation();
         assertEquals(expected, actual);
     }
 
     @Test
-    void setLoudness() {
+    void setVolume() {
         Radio radio = new Radio();
+        int expected = 5;
+        int actual = radio.getCurrentVolume();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextStation() {
+        Radio radio = new Radio(5);
         int expected = 6;
-        int actual = radio.setLoudness();
+        int actual = (radio.getCurrentStation()) + 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void prevStation() {
+        Radio radio = new Radio(5);
+        int expected = 4;
+        int actual = radio.getCurrentStation() - 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void nextVolume() {
+        Radio radio = new Radio();
+        int expected = 0;
+        int actual = (radio.getCurrentVolume()) + 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void prevVolume() {
+        Radio radio = new Radio();
+        int expected = 4;
+        int actual = radio.getCurrentVolume() - 1;
         assertEquals(expected, actual);
     }
 }
