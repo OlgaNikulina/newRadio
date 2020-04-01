da52843646adc17812829b1744776b53a3433480
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-01 13:20:24 +0500

    veiw

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
-        @Test
-        public void shouldCreate() {
-            Radio radio = new Radio();
-            radio.getCurrentStation();
-        }
+    @Test
+    public void shouldCreate() {
+        Radio radio = new Radio();
+        radio.getCurrentStation();
     }
+}
+
 
