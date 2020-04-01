dcbe140c37c39c65f1dd6491bb1e5131463a7f4d
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-01 13:25:28 +0500

    commit

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -16,23 +16,11 @@ public class Radio {
     private int maxVolume = 100;
     private int minVolume = 0;
     private boolean on;
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
-
-    public void setCurrentStation(int currentStation) {
-        this.currentStation = currentStation;
-    }
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
-    public void setCurrentVolume(int currentVolume) {
-        this.currentVolume = currentVolume;
-    }
 }
+
+
+
+
 
 
 
